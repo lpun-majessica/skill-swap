9828e6526818e275aaa5d5db32ce5bfbe17d8ea6
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-04-24 12:09:37 +0700

    Add custom colors

--- src/app/home.js
+++ src/app/home.js
@@ -2,7 +2,9 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center min-h-screen p-24">
       <div>
-        <h1 className="text-4xl font-bold mb-4">Landing page</h1>
+        <h1 className="text-4xl text-ss-red-505 font-bold mb-4">
+          Landing page
+        </h1>
       </div>
     </div>
   );

3aec33ac97e5edcfbea9adf244cd5acc833622e0
Majestique <nhi.lpu1205@gmail.com>
2025-04-24 12:34:34 +0700

    feat: combine pending and connected buttons into 1 component

--- src/components/common/Buttons.jsx
+++ src/components/common/Buttons.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
 	Popover,
@@ -5,7 +7,7 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { ChevronUp, UserRoundPlus } from "lucide-react";
+import { ChevronUp, UserRoundPlus, X } from "lucide-react";
 
 const sharedClass = "text-xs lg:text-sm w-24 h-8 lg:w-28 lg:h-10 rounded-4xl";
 
@@ -13,6 +15,7 @@ export function ConnectButton() {
 	return (
 		<Button
 			className={`${sharedClass} bg-red-600 text-stone-100 hover:bg-red-700/80 hover:cursor-pointer dark:bg-red-700 dark:hover:bg-red-800/90 dark:text-stone-200`}
+			onClick={() => {}}
 		>
 			Connect <UserRoundPlus />
 		</Button>
@@ -29,34 +32,41 @@ export function AcceptButton() {
 	);
 }
 
-export function ConnectedButton() {
-	return (
-		<Popover>
-			<PopoverTrigger
-				className={`${sharedClass} flex flex-row justify-center items-center gap-1 bg-stone-200/90 border-2 border-stone-300 text-stone-700 hover:bg-stone-300/80 hover:cursor-pointer dark:bg-stone-700/90 dark:hover:bg-stone-600/80 dark:border-stone-600/70 dark:text-stone-200`}
-			>
-				Connected <ChevronUp className="size-4 rotate-180" />
-			</PopoverTrigger>
-			<PopoverContent className="p-2 size-fit bg-stone-100 dark:bg-stone-600">
-				<Button className="w-35 text-xs lg:w-40 lg:text-sm bg-transparent text-stone-700 hover:bg-red-200 dark:hover:bg-red-400/70 dark:text-stone-200">
-					Remove connection
-				</Button>
-			</PopoverContent>
-		</Popover>
-	);
-}
+export function DropDownButton({ variant, username }) {
+	const {
+		buttonText,
+		popoverOption,
+		dialogText,
+		dialogEndingMark,
+		confirmButtonText,
+	} = {
+		pending: {
+			buttonText: "Pending",
+			popoverOption: "Cancel request",
+			dialogText: "Cancel pending request with",
+			dialogEndingMark: "?",
+			confirmButtonText: "Cancel request",
+		},
+		connected: {
+			buttonText: "Connected",
+			popoverOption: "Remove connection",
+			dialogText: "Remove connection with",
+			dialogEndingMark: "?",
+			confirmButtonText: "Remove",
+		},
+	}[variant];
 
-export function PendingButton() {
 	return (
 		<Popover>
 			<PopoverTrigger
 				className={`${sharedClass} flex flex-row justify-center items-center gap-1 bg-stone-200/90 border-2 border-stone-300 text-stone-700 hover:bg-stone-300/80 hover:cursor-pointer dark:bg-stone-700/90 dark:hover:bg-stone-600/80 dark:border-stone-600/70 dark:text-stone-200`}
 			>
-				Pending <ChevronUp className="size-4 rotate-180" />
+				{buttonText} <ChevronUp className="size-4 rotate-180" />
 			</PopoverTrigger>
+
 			<PopoverContent className="p-2 size-fit bg-stone-100  dark:bg-stone-600">
-				<Button className="w-35 text-xs lg:w-40 lg:text-sm bg-transparent text-stone-700  hover:bg-red-200 dark:hover:bg-red-400/70 dark:text-stone-200">
-					Cancel request
+				<Button className="w-35 p-2 rounded-sm text-xs lg:w-40 lg:text-sm bg-transparent text-stone-700 hover:cursor-pointer hover:bg-red-200 dark:hover:bg-red-400/70 dark:text-stone-200">
+					{popoverOption}
 				</Button>
 			</PopoverContent>
 		</Popover>

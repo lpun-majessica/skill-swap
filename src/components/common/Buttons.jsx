5a4ebabb23148a4c0c4c6df3aef5a70e6d927a7f
Majestique <nhi.lpu1205@gmail.com>
2025-04-25 12:41:14 +0700

    feat: update custom class for Button and SkillBadge, merge Accept and Connected buttons

--- src/components/common/Buttons.jsx
+++ src/components/common/Buttons.jsx
@@ -1,34 +1,25 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button as ButtonTemplate } from "@/components/ui/button";
 import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { ChevronUp, UserRoundPlus, X } from "lucide-react";
+import { ChevronUp } from "lucide-react";
 
 const sharedClass = "text-xs lg:text-sm w-24 h-8 lg:w-28 lg:h-10 rounded-4xl";
 
-export function ConnectButton() {
+export function Button({ text, handleClick, children }) {
 	return (
-		<Button
-			className={`${sharedClass} bg-red-600 text-stone-100 hover:bg-red-700/80 hover:cursor-pointer dark:bg-red-700 dark:hover:bg-red-800/90 dark:text-stone-200`}
-			onClick={() => {}}
+		<ButtonTemplate
+			className={`${sharedClass} bg-ss-red-505 text-ss-light-555 hover:bg-ss-red-404 active:bg-ss-red-404/70 hover:cursor-pointer dark:bg-ss-red-404 dark:hover:bg-ss-red-404/70 dark:active:bg-ss-red-404/50  dark:text-ss-light-222`}
+			onClick={handleClick}
 		>
-			Connect <UserRoundPlus />
-		</Button>
-	);
-}
-
-export function AcceptButton() {
-	return (
-		<Button
-			className={`${sharedClass} bg-red-600 text-stone-100 hover:bg-red-700/80 hover:cursor-pointer dark:bg-red-700 dark:hover:bg-red-800/90 dark:text-stone-200`}
-		>
-			Accept
-		</Button>
+			{text}
+			{children}
+		</ButtonTemplate>
 	);
 }
 
@@ -59,15 +50,15 @@ export function DropDownButton({ variant, username }) {
 	return (
 		<Popover>
 			<PopoverTrigger
-				className={`${sharedClass} flex flex-row justify-center items-center gap-1 bg-stone-200/90 border-2 border-stone-300 text-stone-700 hover:bg-stone-300/80 hover:cursor-pointer dark:bg-stone-700/90 dark:hover:bg-stone-600/80 dark:border-stone-600/70 dark:text-stone-200`}
+				className={`${sharedClass} flex flex-row justify-center items-center gap-1 bg-ss-light-555 border-2 border-ss-light-222 text-ss-black-444 hover:bg-ss-light-222 hover:cursor-pointer dark:bg-ss-black-131 dark:hover:bg-ss-black-444 dark:border-ss-black-444 dark:text-ss-light-555`}
 			>
 				{buttonText} <ChevronUp className="size-4 rotate-180" />
 			</PopoverTrigger>
 
-			<PopoverContent className="p-2 size-fit bg-stone-100  dark:bg-stone-600">
-				<Button className="w-35 p-2 rounded-sm text-xs lg:w-40 lg:text-sm bg-transparent text-stone-700 hover:cursor-pointer hover:bg-red-200 dark:hover:bg-red-400/70 dark:text-stone-200">
+			<PopoverContent className="p-2 size-fit bg-ss-light-777  dark:bg-ss-black-171">
+				<ButtonTemplate className="w-35 p-2 rounded-sm text-xs lg:w-40 lg:text-sm bg-transparent text-ss-black-717 hover:cursor-pointer hover:bg-ss-red-ABA dark:hover:bg-ss-red-666/80 dark:text-ss-light-555">
 					{popoverOption}
-				</Button>
+				</ButtonTemplate>
 			</PopoverContent>
 		</Popover>
 	);

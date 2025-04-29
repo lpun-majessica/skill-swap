d94c2310c2535190d86d531a8607da39bd1e0371
ngocanh01 <nanh5605@gmail.com>
2025-04-29 13:23:10 +0700

    responsive for mobile

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,7 +10,6 @@ export default function LoginPage() {
 
   const handleLogin = (e) => {
     e.preventDefault(); // prevent reload
-  
     if (login(username)) {
       router.push('/explore');
     } else {
@@ -20,31 +19,33 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen grid grid-cols-1 md:grid-cols-5 font-sans">
-      {/* Left Side */}
-      <div className="bg-red-600 text-white justify-center relative col-span-2">
+      {/* Left Side: Hidden on Mobile */}
+      <div className="bg-red-600 text-white justify-center relative col-span-2 md:block hidden">
         <div className="p-9 text-lg font-bold">SkillSwap</div>
         <div className="text-center">
           <h1 className="text-2xl font-bold mb-4 w-full">Welcome back!</h1>
           <p className="text-xl w-full">Time to enhance your tech skills</p>
-        </div>       
+        </div>
         <div className="relative h-96 overflow-visible">
           <img
             src="/pfp/login.png"
             alt="Pending card"
             className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-[300%] h-auto object-contain "
-          />          
+          />
         </div>
       </div>
 
       {/* Right Side */}
-      <div className="bg-white flex justify-center relative col-span-3 dark dark:bg-[oklch(0.145_0_0)]">
-        <div className="absolute right-5 p-10">
-          <ModeToggle/>
+      <div className="bg-white flex justify-center relative col-span-3 md:col-span-3 dark dark:bg-[oklch(0.145_0_0)]">        
+        <div className="absolute right-5 p-10 z-10 flex justify-between items-center w-full">
+          <div className="text-left"><div className="p-9 text-lg font-bold md:hidden visible text-left">SkillSwap</div></div>
+          
+          <div className="text-right"><ModeToggle /></div>          
         </div>
-        <div className="flex flex-col justify-center items-center px-10 py-20">
+        <div className="flex flex-col justify-center items-center px-10 py-20 w-full md:w-auto">
           <h2 className="text-2xl font-bold mb-6">Sign in</h2>
           <form className="w-full max-w-sm" onSubmit={handleLogin}>
-          <label htmlFor="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input
               type="username"
               placeholder="your username"

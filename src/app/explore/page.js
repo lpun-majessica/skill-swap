09c4ee4acf8ee3bca0570347986b9308d41ac9f0
Majestique <nhi.lpu1205@gmail.com>
2025-04-30 17:36:56 +0700

    fix: move client-side rendering into filter

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,20 +1,11 @@
-"use client";
-import Filter from "@/components/ui/filter";
+import Filter from "@/components/filter";
 
 export default function ExplorePage() {
-  return (
-    <div className="flex flex-row items-start min-h-screen px-20 py-10">
-      {/* Filter */}
-      <div className="flex flex-col items-center mr-8">
-        {/* <h1 className="text-4xl font-bold mb-4">Filter</h1> */}
-        {/* <p className="mb-8">Select your preferences.</p> */}
-        {/* <div className="bg-white shadow-md rounded-lg p-4 w-full"> */}
-          {/* <h2 className="text-xl font-bold mb-4">Filter Options</h2> */}
-          
-          <Filter/>
-        </div>
-
-      {/* Main content */}
-    </div>
-  );
+	return (
+		<div className="flex flex-row items-start min-h-screen px-20 py-10">
+			<div className="flex flex-col items-center mr-8">
+				<Filter />
+			</div>
+		</div>
+	);
 }

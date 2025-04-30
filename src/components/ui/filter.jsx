546609f59aefde852ab5bc7eebda93b975ebfcc2
Diem My Le <119517712+diemmylelelele@users.noreply.github.com>
2025-04-30 08:32:29 +0700

    Adjust color of filter components

--- src/components/ui/filter.jsx
+++ src/components/ui/filter.jsx
@@ -57,7 +57,7 @@ export default function Filter({ onFilterChange }) {
       <div className="sm:hidden w-full px-4 pt-4">
         <button
           onClick={() => setIsMobileFilterOpen(true)}
-          className="flex items-center gap-2 px-4 py-2 dark:bg-gray-500 bg-gray-200 rounded-md text-sm font-semibold"
+          className="flex items-center text-base gap-2 px-4 py-2 dark:bg-ss-black-929 bg-ss-light-FFF rounded-md font-semibold"
         >
           <FilterIcon size={16} />
           Filter
@@ -65,7 +65,7 @@ export default function Filter({ onFilterChange }) {
       </div>
 
       {/* Desktop view */}
-      <div className="hidden sm:flex flex-col w-[290px] h-[625px] rounded-2xl shadow-md bg-white dark:bg-black p-4">
+      <div className="hidden sm:flex flex-col w-[254px] h-[625px] rounded-2xl shadow-md bg-ss-light-FFF dark:bg-ss-black-929 p-4">
         <TopBar handleClearAll={handleClearAll} />
         <FilterBody
           searchTerm={searchTerm}
@@ -79,12 +79,12 @@ export default function Filter({ onFilterChange }) {
 
       {/* Mobile Modal */}
       {isMobileFilterOpen && (
-        <div className="fixed inset-0 z-50 bg-white dark:bg-black flex flex-col p-4">
+        <div className="fixed inset-0 z-50 bg-ss-light-FFF dark:bg-ss-black-929 flex flex-col p-4">
           {/* Topbar inside mobile filter */}
           <div className="flex items-center justify-between mb-4">
             <div className="flex items-center gap-2">
-              <FilterIcon size={18} className="text-black dark:text-gray-300" />
-              <h2 className="font-semibold text-lg text-black dark:text-gray-200">
+              <FilterIcon size={16} className="text-black dark:text-gray-300" />
+              <h2 className="font-semibold text-base text-black dark:text-white">
                 Filter
               </h2>
             </div>
@@ -106,14 +106,14 @@ export default function Filter({ onFilterChange }) {
           <div className="mt-auto grid grid-cols-2 gap-2 pt-4">
         <button
           onClick={handleClearAll}
-          className="text-sm w-full py-2 bg-gray-200 dark:bg-gray-400 dark:text-white text-black rounded-md font-semibold hover:bg-gray-200 dark:hover:bg-gray-500 transition-colors"
+          className="text-sm py-1 px-10 flex items-center justify-center bg-ss-light-222 dark:bg-ss-black-444 dark:text-white text-black rounded-md font-semibold hover:bg-gray-100 dark:hover:bg-ss-black-44 transition-colors"
         >
           Clear all
         </button>
   
         <button
           onClick={() => setIsMobileFilterOpen(true)}
-          className="text-sm w-full flex items-center justify-center gap-1 py-2 bg-gray-100 dark:bg-gray-400 text-black dark:text-white rounded-md font-semibold hover:bg-gray-200 dark:hover:bg-gray-500 transition-colors"
+          className="text-sm py-1 px-10 flex items-center justify-center gap-1 bg-ss-light-222 dark:bg-ss-black-444 text-black dark:text-white rounded-md font-semibold hover:bg-gray-100 dark:hover:bg-ss-black-444 transition-colors"
         >
           Filter
         </button>
@@ -128,14 +128,14 @@ function TopBar({ handleClearAll }) {
   return (
     <div className="hidden sm:flex items-center justify-between mb-4">
       <div className="flex items-center gap-2">
-        <FilterIcon size={15} className="text-gray-600 dark:text-gray-300" />
-        <h2 className="font-semibold text-base sm:text-lg text-black dark:text-gray-200">
+        <FilterIcon size={16} className="text-black dark:text-white" />
+        <h2 className="font-semibold text-base sm:text-lg text-black dark:text-white">
           Filter
         </h2>
       </div>
       <button
         onClick={handleClearAll}
-        className="text-red-400 text-xs sm:text-sm hover:underline"
+        className="text-ss-red-666 text-xs sm:text-sm hover:underline"
       >
         Clear all
       </button>
@@ -159,37 +159,39 @@ function FilterBody({
           placeholder="Search skills"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="rounded-full bg-gray-100 dark:bg-black text-left text-xs sm:text-sm text-black dark:text-gray-200"
+          className="rounded-full bg-ss-light-555 dark:bg-ss-black-121 text-left text-xs sm:text-sm text-black dark:text-gray-200"
         />
       </div>
 
-      <hr className="border-t border-dashed border-gray-400 dark:border-gray-400 my-2" />
+      <hr className="border-t border-dashed border-gray-400 dark:border-gray-400 my-1" />
 
       {/* Skill list */}
       <div className="flex flex-col overflow-y-auto pr-1 sm:pr-2 space-y-2">
         {/* Sticky header */}
-        <div className="grid grid-cols-3 items-center gap-1 sm:gap-2 px-1 sm:px-2 py-2 sticky top-0 bg-white dark:bg-black z-10">
-          <div className="text-xs sm:text-sm font-semibold text-left text-black dark:text-white">Skills</div>
-          <div className="text-xs sm:text-sm font-semibold text-center text-gray-400 dark:text-gray-200">Teach</div>
-          <div className="text-xs sm:text-sm font-semibold text-center text-gray-400 dark:text-gray-200">Learn</div>
+        <div className="grid grid-cols-[3.5fr_1fr_1fr] gap-1 sm:gap-2 px-1 sm:px-2 py-2 sticky top-0 bg-white dark:bg-ss-black-929 z-10">
+          <div className="text-base font-semibold text-left text-black dark:text-white">Skills</div>
+          <div className="text-sm  text-center text-gray-300 dark:text-gray-200">Teach</div>
+          <div className="text-sm   text-center text-gray-300 dark:text-gray-200">Learn</div>
         </div>
 
         {sortedSkills.map((skill) => (
           <div
             key={skill}
-            className="grid grid-cols-3 items-center gap-1 sm:gap-2 px-1 sm:px-2 py-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-200 transition-colors duration-300"
+            className="grid grid-cols-[3.5fr_1fr_1fr] gap-1 sm:gap-2 px-1 sm:px-2 py-2 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-300 transition-colors duration-300"
           >
             <div className="flex justify-start items-center">
-              <span className="bg-gray-200 dark:bg-gray-500 whitespace-nowrap rounded-full px-2 py-0.5 text-[9px] sm:text-[10px] font-medium text-gray-800 dark:text-gray-200">
-                {skill}
-              </span>
+            <span className="bg-ss-light-222 dark:bg-ss-black-444 whitespace-nowrap rounded-full px-2 py-0.5 text-[12px] sm:text-[19px] md:text-[11px] font-medium text-black dark:text-white">
+               {skill}
+            </span>
+
             </div>
             <div className="flex justify-center items-center">
               <input
                 type="checkbox"
                 checked={selectedTeach.includes(skill)}
                 onChange={() => handleCheckboxChange(skill, "teach")}
-                className="w-3.5 h-3.5 sm:w-4 sm:h-4 flex-shrink-0 rounded border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-500 accent-red-400 focus:ring-0"
+                className="w-3.5 h-3.5 sm:w-4 sm:h-4 flex-shrink-0 rounded border-gray-300 dark:border-ss-black-717 bg-white dark:bg-ss-black-717 focus:ring-0 accent-ss-red-666 "
+
               />
             </div>
             <div className="flex justify-center items-center">
@@ -197,7 +199,7 @@ function FilterBody({
                 type="checkbox"
                 checked={selectedLearn.includes(skill)}
                 onChange={() => handleCheckboxChange(skill, "learn")}
-                className="w-3.5 h-3.5 sm:w-4 sm:h-4 flex-shrink-0 rounded border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-500 accent-red-400 focus:ring-0"
+                className="w-3.5 h-3.5 sm:w-4 sm:h-4 flex-shrink-0 rounded border-gray-300 dark:border-ss-black-717 bg-white dark:bg-ss-black-717 focus:ring-0 accent-ss-red-666"
               />
             </div>
           </div>

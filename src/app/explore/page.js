3ffa4ac4a7b17ae8a00b0ff352828175f6bb4691
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-04-30 19:22:25 +0700

    fix: fix syntax error in explore page

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,10 +1,11 @@
 import Filter from "@/components/filter";
 
 export default function ExplorePage() {
-	return (
-		<div className="flex flex-row items-start min-h-screen px-20 py-10">
-			<div className="flex flex-col items-center mr-8">
-				<Filter />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row items-start min-h-screen px-20 py-10">
+      <div className="flex flex-col items-center mr-8">
+        <Filter />
+      </div>
+    </div>
+  );
+}

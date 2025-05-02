53b0578a2264bdac32d5e4a101e472dfafa4b41b
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-05-02 20:27:29 +0700

    fix: move recommendation logic to data-context to search all users

--- src/components/user-list/recommended-user-list.jsx
+++ src/components/user-list/recommended-user-list.jsx
@@ -5,34 +5,14 @@ import { useAuthContext } from "@/contexts/auth-context";
 import { UserList } from "./user-list";
 
 export function RecommendedUserList() {
-	const { currentUser } = useAuthContext();
-	const { getFilteredUsers } = useDataContext();
+  const { currentUser } = useAuthContext();
+  const { getFilteredUsers, users } = useDataContext();
 
-	if (!currentUser) return null;
+  if (!currentUser) return <UserList users={users} />;
 
-	const filteredUsers = getFilteredUsers(currentUser.id);
-	const displayUsers = filteredUsers.sort(
-		(userA, userB) =>
-			countSimilarSkills(userB.skillsToTeach, userB.skillsToLearn) -
-			countSimilarSkills(userA.skillsToTeach, userA.skillsToLearn)
-	);
+  // recommended users by default
+  // or filtered users when filters or search are applied
+  const displayUsers = getFilteredUsers(currentUser.id);
 
-	return <UserList users={displayUsers} />;
-
-	function countSimilarSkills(skillsToTeach, skillsToLearn) {
-		return (
-			compare(skillsToTeach, currentUser.skillsToLearn) +
-			compare(skillsToLearn, currentUser.skillsToTeach)
-		);
-
-		function compare(targetSkills, userSkills) {
-			let result = 0;
-			if (targetSkills && userSkills) {
-				targetSkills.forEach(
-					(skill) => (result += userSkills.includes(skill) ? 1 : 0)
-				);
-			}
-			return result;
-		}
-	}
+  return <UserList users={displayUsers} />;
 }

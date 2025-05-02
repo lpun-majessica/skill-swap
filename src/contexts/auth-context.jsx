809f166cb5927fb74fb2dbef7d3b48ea6cdbd5d7
Nguyennhuhha <nhuha3110@gmail.com>
2025-05-02 17:28:24 +0700

    feat: add updateCurrentUser logic

--- src/contexts/auth-context.jsx
+++ src/contexts/auth-context.jsx
@@ -5,19 +5,29 @@ import { login as loginUser, logout as logoutUser, getUser } from '@/utils/auth'
 
 const AuthContext = createContext();
 
-export function AuthProvider({children}){
+export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
         const storedUser = getUser();
-        if(storedUser){
+        if (storedUser) {
             setCurrentUser(storedUser);
         }
     }, []);
 
+    const updateCurrentUser = (updatedFields) => {
+        const storedUser = getUser();
+        if (!storedUser) return;
+    
+        const updatedUser = { ...storedUser, ...updatedFields };
+    
+        localStorage.setItem('user', JSON.stringify(updatedUser));
+        setCurrentUser(updatedUser);
+    };
+
     const login = (username) => {
         const success = loginUser(username);
-        if(success){
+        if (success) {
             const loggedInUser = getUser();
             setCurrentUser(loggedInUser);
         }
@@ -30,16 +40,16 @@ export function AuthProvider({children}){
     };
 
     return (
-        <AuthContext.Provider value={{currentUser, login, logout}}>
+        <AuthContext.Provider value={{ currentUser, updateCurrentUser, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
 }
- 
+
 export function useAuthContext() {
     const context = useContext(AuthContext);
     if (!context) {
-      throw new Error('useAuthContext must be used within an AuthProvider');
+        throw new Error('useAuthContext must be used within an AuthProvider');
     }
     return context;
-  }
+}

872fcb999d3fbd66df261195e49f8261824d08ac
ngocanh01 <nanh5605@gmail.com>
2025-05-03 10:47:09 +0700

    update code

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -25,28 +25,34 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen grid grid-cols-1 md:grid-cols-5 font-sans relative overflow-hidden">
       {/* Left Side: Hidden on Mobile */}
-      <div className="bg-red-600 text-white relative col-span-2 md:block hidden overflow-hidden">
-        <div className="p-9 text-lg font-bold">SkillSwap</div>
+      <div className="bg-red-600 text-white relative col-span-2 md:block hidden overflow-visible">
+        <div className="p-9 text-lg font-bold flex items-center gap-2">
+          <Logo />
+          <span>SkillSwap</span>
+        </div>
 
         <div className="text-center relative z-10 flex flex-col items-center justify-center">
           <h1 className="text-2xl font-bold mb-4">Welcome back!</h1>
           <p className="text-xl">Time to enhance your tech skills</p>
         </div>
 
-        <div className="absolute inset-x-0 bottom-0 h-full pointer-events-none z-20 overflow-hidden">
-          <img
-            src="/pfp/login-1.svg"
-            alt="Pending card"
-            className="absolute bottom-[-70px] left-1/2 translate-x-[-50%] w-[120%] max-w-none h-auto object-contain"
-          />
-        </div>
+        <div className="absolute inset-x-0 bottom-0 h-full pointer-events-none z-50 overflow-visible">
+  <img
+    src="/pfp/login-1.svg"
+    alt="Pending card"
+    className="absolute bottom-[-70px] left-1/2 translate-x-[-50%] w-[120%] max-w-none h-auto object-contain"
+  />
+</div>
       </div>
 
       {/* Right Side */}
       <div className="bg-white flex justify-center relative col-span-3 md:col-span-3 dark dark:bg-[oklch(0.145_0_0)] z-0">
         <div className="absolute right-5 p-10 z-10 flex justify-between items-center w-full">
           <div className="text-left">
-            <div className="p-9 font-bold md:hidden visible text-left"><Logo /></div>
+            <div className="p-9 font-bold md:hidden visible text-left flex items-center gap-2">
+              <Logo />
+              <span>SkillSwap</span>
+            </div>
           </div>
           <div className="text-right">
             <ModeToggle />

aeba882dab70456862a346590353700beaabf3a3
Majestique <nhi.lpu1205@gmail.com>
2025-06-27 13:08:10 +0700

    feat: loading icons

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,7 +1,9 @@
+import LoadingIcon from "@/components/common/loading-icon";
+
 export default function Loading() {
   return (
-    <div className="flex min-h-95 items-center justify-center text-3xl">
-      Loading ...
+    <div className="flex min-h-95 items-center justify-center">
+      <LoadingIcon />
     </div>
   );
 }
